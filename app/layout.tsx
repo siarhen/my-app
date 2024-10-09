1205e5cda1c0811649e9b36be65457e5fe9140cf
Sergey Rachitskiy <s_rachitskiy@lesta.group>
2024-10-09 16:12:14 +0300

    213

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export const metadata = {
       siteName: '123',
       images: [
           {
-              url: 'https://cdn.devreality.ru/assets/logo_DevReality_black.png',
+              url: 'https://cdn.devreality.ru/assets/466.jpg',
               alt: 'image alt',
           },
       ],
@@ -45,7 +45,7 @@ export const metadata = {
     description:
       "A guide on how to optimize SEO with static and dynamic metatags using Next.js 13's new Metadata API.",
     images: [
-      "https://cdn.devreality.ru/assets/logo_DevReality_black.png",
+      "https://cdn.devreality.ru/assets/466.jpg",
     ],
   },
   other: {
