bda2ce84254540a88ffc0b2e3edd35ebfa33c38b
Sergey Rachitskiy <s_rachitskiy@lesta.group>
2024-10-09 16:04:19 +0300

    123213

---
+++ app/post/page.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Metadata, ResolvingMetadata } from "next";
+
+export async function generateMetadata(
+    { params }: { params: {shortUrl: string} }, parent: ResolvingMetadata
+): Promise<Metadata> {
+    // const currentArticle: Article = await getEntityShortUrl(params.shortUrl, entityValue.articles);
+    const previousImages = (await parent).openGraph?.images || [];
+    
+    return {
+        title: 'ARTICLLLE',
+        description: 'description of that article',
+        openGraph: {
+            title: 'ARTICLLLE',
+            description: 'description of that article',
+            images: ['https://devreality.ru/article/davankovspasi/', ...previousImages]
+        },
+    };
+}
+
+const Page = () => {
+    return (
+        <div>
+            LOOOOL is it page
+        </div>
+    );
+}
+
+export default Page;
