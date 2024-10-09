630007771f51136d550f9bf8924f7bf4c41c05cd
Sergey Rachitskiy <s_rachitskiy@lesta.group>
2024-10-09 16:06:00 +0300

    123

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -13,7 +13,7 @@ export async function generateMetadata(
         openGraph: {
             title: 'ARTICLLLE',
             description: 'description of that article',
-            images: ['https://devreality.ru/article/davankovspasi/', ...previousImages]
+            images: ['https://devreality.ru/article/davankovspasi/']
         },
     };
 }
