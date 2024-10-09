9ac2c1a5573b2088bff9caa2bc0f9505dee4afa6
Sergey Rachitskiy <s_rachitskiy@lesta.group>
2024-10-09 16:09:36 +0300

    3123

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -13,7 +13,7 @@ export async function generateMetadata(
         openGraph: {
             title: 'ARTICLLLE',
             description: 'description of that article',
-            images: ['https://cdn.devreality.ru/assets/466.jpg', ...previousImages]
+            images: ['https://cdn.devreality.ru/assets/466.jpg']
         },
     };
 }
