0970aa6d20e52a71bcae9cc687d3e854059cf628
Sergey Rachitskiy <s_rachitskiy@lesta.group>
2024-10-10 11:54:08 +0300

    4 14

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -5,15 +5,15 @@ export async function generateMetadata(
     { params }: { params: {shortUrl: string} }, parent: ResolvingMetadata
 ): Promise<Metadata> {
     // const currentArticle: Article = await getEntityShortUrl(params.shortUrl, entityValue.articles);
-    const previousImages = (await parent).openGraph?.images || [];
-    
+    const previousOGImages = (await parent).openGraph?.images || [];
+    const previousTwitterImages = (await parent).twitter?.images || [];
     return {
         title: 'ARTICLLLE',
         description: 'description of that article',
         openGraph: {
             title: 'ARTICLLLE',
             description: 'description of that article',
-            images: ['https://cdn.devreality.ru/assets/466.jpg', ...previousImages]
+            images: ['https://cdn.devreality.ru/assets/466.jpg', ...previousOGImages]
         },
         twitter: {
             card: "summary_large_image",
@@ -21,7 +21,7 @@ export async function generateMetadata(
             description:
               "description of that article",
             images: [
-              "https://cdn.devreality.ru/assets/466.jpg",
+              "https://cdn.devreality.ru/assets/466.jpg", ...previousTwitterImages
             ],
           },
     };
