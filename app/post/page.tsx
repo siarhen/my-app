7cc6fd5dbd33048566692670bd82b8299caa0002
Sergey Rachitskiy <s_rachitskiy@lesta.group>
2024-10-10 11:17:22 +0300

    21321321

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -22,7 +22,7 @@ export async function generateMetadata(
             description:
               "description of that article",
             images: [
-              "https://cdn.devreality.ru/assets/466.jpg",
+              "https://previews.dropbox.com/p/thumb/ACYtyuDgTSBeQe2LfIvsESd1H9w-K9IkezYmohI1su4P-uJhgbuMiudGkfjAUhj98RlPysGaGvaPQdCz46Ge0XmnYUJxVJnePcMQTFMbodVFkP4nMTIDRgJLyXjcPi0Z7iiM4mNd8oDiPw7WKAGuiD28PmXHM7_WZbHcQ5bYQ-WSm4YAD4E-x6nFJpE2Iv0ogaJSQ9ci5u1RVZYZbPYw8jzld67IgejBVjMCCxq6XwNgHYJbISfLlBv4rfGtJ5hbfUBku3UZlP1Wd456a4pnG6MqpzhRd25qjK2u-yNEmDadvrm2mhhRAoBN1KwhODwG_eTYf8_GBKfpx_VidTXQlHXN/p.png",
             ],
           },
     };
